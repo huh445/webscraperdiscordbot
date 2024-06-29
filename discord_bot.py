da546223f4a7674cf63b91535e2855522ede8211
huh445 <127354663+huh445@users.noreply.github.com>
2024-06-29 13:48:47 +1000

    update

--- discord_bot.py
+++ discord_bot.py
@@ -1,19 +1,29 @@
 import discord
+import aiohttp
+import certifi
+import ssl
 from discord.ext import commands
 from web_scraper import WebScraper
-
 class Discord:
 
     def __init__(self):
+
         intents = discord.Intents.default()
         intents.message_content = True
         bot = commands.Bot(command_prefix=".", intents=intents)
 
         self.web_scraper = WebScraper()
 
+        ssl_context = ssl.create_default_context(cafile=certifi.where())
+
+
         @bot.event
         async def on_ready():
             print(f"Logged in as {bot.user}")
+            async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=ssl_context)) as session:
+                async with session.get('https://discord.com/api/v10') as response:
+                    print(await response.text())
+
 
         @bot.command()
         async def page_get(ctx):
